c98e93562e955c8629f78a93b391bdcab93d21ee
rupam swain <rupamswain1@gmail.com>
2024-03-25 23:48:26 +0530

    callback

--- src/routes/api/auth/login/+server.ts
+++ src/routes/api/auth/login/+server.ts
@@ -18,7 +18,9 @@ const challenge = pkce.create();
 
 const scope =
 	'ugc-image-upload user-modify-playback-state user-read-playback-state user-read-currently-playing user-follow-modify user-follow-read user-read-recently-played user-read-playback-position user-top-read playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private app-remote-control streaming user-read-email user-read-private user-library-modify user-library-read';
-export const GET: RequestHandler = () => {
+export const GET: RequestHandler = ({ cookies }) => {
+	cookies.set('spotify_auth_state', state);
+	cookies.set('spotify_auth_challenge_verifier', challenge.code_verifier);
 	throw redirect(
 		307,
 		`https://accounts.spotify.com/authorize?${new URLSearchParams({
@@ -27,7 +29,7 @@ export const GET: RequestHandler = () => {
 			scope,
 			redirect_uri: `${BASE_URL}/api/auth/callback`,
 			state,
-			code_challenge_method: 'S256',
+			code_challenge_method: 'S25.env6',
 			code_challenge: challenge.code_challenge
 		})}`
 	);
