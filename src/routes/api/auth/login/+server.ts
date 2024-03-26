09405973b0949ae5c9eabbbfd16b5729e76aa11a
rupam swain <rupamswain1@gmail.com>
2024-03-26 23:26:25 +0530

    refactor

--- src/routes/api/auth/login/+server.ts
+++ src/routes/api/auth/login/+server.ts
@@ -15,12 +15,12 @@ const generateRandomString = (length: number) => {
 
 const state = generateRandomString(16);
 const challenge = pkce.create();
-
+console.log('login', `${BASE_URL}/api/auth/callback`);
 const scope =
 	'ugc-image-upload user-modify-playback-state user-read-playback-state user-read-currently-playing user-follow-modify user-follow-read user-read-recently-played user-read-playback-position user-top-read playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private app-remote-control streaming user-read-email user-read-private user-library-modify user-library-read';
 export const GET: RequestHandler = ({ cookies }) => {
-	cookies.set('spotify_auth_state', state);
-	cookies.set('spotify_auth_challenge_verifier', challenge.code_verifier);
+	cookies.set('spotify_auth_state', state, { path: '/' });
+	cookies.set('spotify_auth_challenge_verifier', challenge.code_verifier, { path: '/' });
 	throw redirect(
 		307,
 		`https://accounts.spotify.com/authorize?${new URLSearchParams({
@@ -29,7 +29,7 @@ export const GET: RequestHandler = ({ cookies }) => {
 			scope,
 			redirect_uri: `${BASE_URL}/api/auth/callback`,
 			state,
-			code_challenge_method: 'S25.env6',
+			code_challenge_method: 'S256',
 			code_challenge: challenge.code_challenge
 		})}`
 	);
