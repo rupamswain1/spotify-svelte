e2a49de82b6df478ebf7e981c34cc12a57e7331b
rupam swain <rupamswain1@gmail.com>
2024-04-05 23:18:57 +0530

    redirect logic

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 	import '../styles/main.scss';
+	import type {LayoutData} from './$types'; 
+
+	export let data: LayoutData;
 </script>
+
+{data.user?.display_name}
 
 <slot />
