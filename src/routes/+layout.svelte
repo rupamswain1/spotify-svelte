b6c71ed576cc7145016efe9bffd9fe8b166648b3
rupam swain <rupamswain1@gmail.com>
2024-04-16 19:30:50 +0530

    button styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-	import LogoutButton from '$components/LogoutButton.svelte';
+	import { LogoutButton, Button } from '$components';
 	import '../app.css';
 	import '../styles/main.scss';
-	import type {LayoutData} from './$types'; 
+	import type { LayoutData } from './$types';
 	export let data: LayoutData;
 	$: user = data.user;
 </script>
 
+<Button element="a" variant={'outline'} href>Login</Button>
+<Button element="button">Login</Button>
 {#if user}
 	<p>Hello {user.display_name}</p>
-	<LogoutButton/>
+	<LogoutButton />
 {/if}
-
-
 
 <slot />
