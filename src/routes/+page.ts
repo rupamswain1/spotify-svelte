25e336a80db7d95e62c687dfa7b25e5cbe0fa418
rupam swain <rupamswain1@gmail.com>
2024-05-01 23:13:30 +0530

    adding common route

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,7 +2,7 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
 	const res = await fetch('/api/spotify/browse/new-release?limit=6');
-	const res2 = await fetch('/api/spotify/browse/feature-playlists?limit=6');
+	const res2 = await fetch('/api/spotify/browse/featured-playlists?limit=6');
 	console.log(await res.json());
 	console.log(await res2.json());
 };
