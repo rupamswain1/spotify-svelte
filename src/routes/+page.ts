5674b697f099d4b7c53e765f9685f9174955f669
rupam swain <rupamswain1@gmail.com>
2024-05-03 13:35:43 +0530

    spotify home api implementation

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,8 +1,42 @@
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ fetch }) => {
-	const res = await fetch('/api/spotify/browse/new-release?limit=6');
-	const res2 = await fetch('/api/spotify/browse/featured-playlists?limit=6');
-	console.log(await res.json());
-	console.log(await res2.json());
+export const load: PageLoad = async ({ fetch, parent }) => {
+	const { user } = await parent();
+	const newReleases = fetch('/api/spotify/browse/new-releases?limit=6');
+
+	const featuredPlayLists = fetch('/api/spotify/browse/featured-playlists?limit=6');
+	const userPlayLists = fetch(`/api/spotify/users/${user?.id}/playlists?limit=6`);
+
+	const catRes = await fetch(`api/spotify/browse/categories`);
+	const catsResJSON: SpotifyApi.MultipleCategoriesResponse = catRes.ok
+		? await catRes.json()
+		: undefined;
+
+	const randomCats = catsResJSON
+		? catsResJSON.categories.items.sort(() => 0.5 - Math.random()).slice(0, 3)
+		: [];
+	console.log({ randomCats });
+	const randomCatsPromises = randomCats.map((cat) =>
+		fetch(`/api/spotify/browse/categories/${cat.id}/playlists?limit=6`)
+	);
+	const [newReleasesRes, featuredPlayListsRes, userPlayListsRes, ...randomCatsRes] =
+		await Promise.all([newReleases, featuredPlayLists, userPlayLists, ...randomCatsPromises]);
+
+	return {
+		newReleases: newReleasesRes.ok
+			? (newReleasesRes.json() as Promise<SpotifyApi.ListOfNewReleasesResponse>)
+			: undefined,
+		featuredPlaylists: featuredPlayListsRes.ok
+			? (featuredPlayListsRes.json() as Promise<SpotifyApi.ListOfFeaturedPlaylistsResponse>)
+			: undefined,
+		userPlayLists: userPlayListsRes.ok
+			? (userPlayListsRes.json() as Promise<SpotifyApi.ListOfUsersPlaylistsResponse>)
+			: undefined,
+		homeCategories: randomCats,
+		categoriesPlaylists: Promise.all(
+			randomCatsRes.map((res) =>
+				res.ok ? (res.json() as Promise<SpotifyApi.CategoryPlaylistsResponse>) : undefined
+			)
+		)
+	};
 };
--- src/routes/api/spotify/[...path]/+server.ts
+++ src/routes/api/spotify/[...path]/+server.ts
@@ -1,14 +1,14 @@
 import { SPOTIFY_BASE_URL } from '$env/static/private';
-import { error, json, type RequestHandler } from '@sveltejs/kit';
-
+import { error, json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
 export const GET: RequestHandler = async ({ fetch, cookies, params, url }) => {
 	const accessToken = cookies.get('access_token');
-
 	const response = await fetch(`${SPOTIFY_BASE_URL}/${params.path}${url.search}`, {
 		headers: {
 			Authorization: `Bearer ${accessToken}`
 		}
 	});
+
 	const responseJSON = await response.json();
 	if (responseJSON.eror) {
 		throw error(responseJSON.error.status, responseJSON.error.message);
